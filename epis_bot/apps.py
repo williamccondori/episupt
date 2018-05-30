da9b0d2e1af3c9a20b40530c25bfd27ff4a0ed59
William Condori Quispe <williamccondori@gmail.com>
2018-05-29 23:29:41 -0500

    Se crea el proyecto

---
+++ epis_bot/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EpisBotConfig(AppConfig):
+    name = 'epis_bot'
---
+++ epis_web/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EpisWebConfig(AppConfig):
+    name = 'epis_web'
